66475dc28b391f25653222ed12a3347afc65bddf
sash5borys <sash5borys@gmail.com>
2020-06-22 19:50:28 +0300

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello"
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="main_page">
+    <Hello />
+  </div>
+</template>
+
+<script>
+import Hello from "@/components/Hello";
+export default {
+  name: "Main",
+  components: {
+    Hello
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
